26fec1f8b82b26cd1384d6c353a87134d0ac2636
Frederick Adebayo <sprintcorp7@gmail.com>
2023-06-02 15:40:57 +0100

    cors origin

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ import { ClassSerializerInterceptor, ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-});
+  app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter())
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   await app.listen(parseInt(process.env.PORT, 10) || 3000);
