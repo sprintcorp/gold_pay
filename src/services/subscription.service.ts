4816cbae173bd185e3e8c2a933ca15b467b489c7
Frederick Adebayo <sprintcorp7@gmail.com>
2023-06-14 20:35:45 +0100

    update

--- src/services/subscription.service.ts
+++ src/services/subscription.service.ts
@@ -16,50 +16,52 @@ export class SubscriptionService{
 
   async serviceSubscription(subscription: SubscriptionDto, request){
 
+    
     if(request.user.transaction_pin != subscription.transactionPin){
       throw new HttpException('Invalid transaction pin', HttpStatus.FORBIDDEN)
     }
+    // return subscription;
 
-    console.log(request.user);
+    // console.log(request.user);
 
     if(parseFloat(request.user.balance) == 0 || parseFloat(request.user.balance) < parseFloat(subscription.result)){
       throw new HttpException('You have insufficient balance, please deposit to continue this action', HttpStatus.FORBIDDEN)
     }
 
-    subscription.user = request.user._id;
     
-
+    
     const url = new URLSwitch(subscription.type, subscription.network, subscription.amount, 
       subscription.subscriptionNumber, subscription.subPackage ?? '', subscription.smartCard ?? '');
-
+    
+    
     const actionURL = url.getSubscriptionURL();
 
     const response = await this.httpService.axiosRef.get(actionURL);
 
-
+    // return response.data;
     subscription.transactionId = response.data.orderid;
-    // subscription.transactionId = Helper.uniqueRandomNumber(10);
 
     const newBalance = parseFloat(request.user.balance) - parseFloat(subscription.result);
 
     const debitBalance = parseFloat(request.user.debit) + parseFloat(subscription.result);
 
-    // return newBalance;
+  
     delete subscription['result']
 
     await new this.subscriptionModel(subscription).save();
 
-    console.log(newBalance);
+    // console.log(newBalance);
 
     // const user = await this.userModel.findByIdAndUpdate(request.user._id,
     //    {balance:newBalance, debit:debitBalance});
 
-       request.user.balance = newBalance;
-       request.user.debit = debitBalance;
-       await request.user.save();
+    request.user.balance = newBalance;
+    request.user.debit = debitBalance;
+    request.user.username = request.user.username ? request.user.username :
+    'user_'+request.user.firstname+request.user.lastname;
 
-    // return request.user;
-    
+    await request.user.save();
+
     return response.data;
   }
 
